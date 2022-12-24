bda4000e0a0856519cbf311b167a03098661ce3c
Amandeep <aman24january@gmail.com>
2022-12-25 02:18:29 +0530

    Updated config files
    Render ejs home.html file

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000 ;
 
+// Set template engine.
+//app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port : ${port}`);
+    console.log(`Server is listening on port : ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    //Render home page html.
+    res.render('home');
+})
+
