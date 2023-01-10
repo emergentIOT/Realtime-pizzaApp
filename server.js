09498947e5791d6b255281ae1ad5102addc623a5
Amandeep <aman24january@gmail.com>
2023-01-11 01:54:48 +0530

    Created empty cart page

--- server.js
+++ server.js
@@ -9,14 +9,21 @@ const PORT = process.env.PORT || 3000 ;
 //app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+app.use(expressLayout);
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
 });
 
+//Home page
 app.get("/", (req, res) => {
     //Render home page html.
     res.render('home');
+})
+
+//Cart page
+app.get("/cart", (req, res) => {
+    res.render('customers/cart');
 })
 
