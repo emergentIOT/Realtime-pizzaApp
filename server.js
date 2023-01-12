2da909eaef8aeda198599b87268fc1394cbd463f
Amandeep <aman24january@gmail.com>
2023-01-13 02:04:27 +0530

    Setup mongoose connection to mongodb

--- server.js
+++ server.js
@@ -4,13 +4,28 @@ const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000 ;
+const mongoose = require('mongoose');
+
+//MongoDB connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { keepAlive: 1, useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log(`Database connected...`);
+})
+
+connection.on('error', () => {
+    console.log(`Database error...$`);
+})
+
+//Assets
+app.use(express.static('public'));
 
 // Set template engine.
-//app.use(expressLayout);
+app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
-app.use(expressLayout);
-app.use(express.static('public'));
 
 //defining routes
 require('./routes/web')(app);
